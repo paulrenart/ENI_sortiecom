c1c9eb4777b9ade3f8d60c88d5d1a47bb437900a
Paul Renart <paul.renart@lengow.com>
2020-09-15 16:34:45 +0200

    add crsf

--- src/src/Form/SortieFormType.php
+++ src/src/Form/SortieFormType.php
@@ -49,6 +49,13 @@ class SortieFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Sorties::class,
+            // enable/disable CSRF protection for this form
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'Xc3qV4vzpRy4*Mxv'
         ]);
     }
 }
